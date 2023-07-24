7ddb6ad48b5d3d1fdbb485cc04248545acee86a8
Parsa Nasirimehr <p.nasirimehr@gmail.com>
2023-07-24 12:55:49 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TheRogue76/fun-cli
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+)
+
+func rgb(i int) (int, int, int) {
+	f := func(offset int) int {
+		return int(math.Sin(0.1*float64(i)+(2.0*float64(offset))*math.Pi/3)*127 + 128)
+	}
+	return f(0), f(1), f(2)
+}
+
+func print(output []rune) {
+	for j := 0; j < len(output); j++ {
+		r, g, b := rgb(j)
+		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, output[j])
+	}
+	fmt.Println()
+}
+
+func main() {
+	info, _ := os.Stdin.Stat()
+	var output []rune
+
+	if info.Mode()&os.ModeCharDevice != 0 {
+		fmt.Println("The command is intended to work with pipes")
+		fmt.Println("Usage: fortune | gorainbow")
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		input, _, err := reader.ReadRune()
+		if err != nil && err == io.EOF {
+			break
+		}
+		output = append(output, input)
+	}
+
+	print(output)
+}
